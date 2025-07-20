58545de503250df021d4c73e77e2a8f89a8d1cc3
Camilla Thorsen <camilla@thorsen.dev>
2025-07-19 18:43:04 -0700

    api|refactor: Remove C++-based audio transcription

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,3 @@
-export { transcribeAudio } from './audio/transcribe.ts';
 export { getBestClipScore, getClipScoresFromImage } from './image/getClipScoresFromImage.ts';
 export { improveText } from './text/improveText.ts';
 export * from './types.ts';
