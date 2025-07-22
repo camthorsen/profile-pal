51b63c7a30bf80b20ed18dd25890194d056ab8a8
Camilla Thorsen <camilla@thorsen.dev>
2025-07-22 10:49:21 -0700

    ui: Add routing to navbar

--- packages/nextjs/src/components/Header.tsx
+++ packages/nextjs/src/components/Header.tsx
@@ -1,17 +1,19 @@
-import { TheNavBar } from '@/components/TheNavBar.tsx';
+import Link from 'next/link';
 import type { ReactElement } from 'react';
+
+import { TheNavBar } from '@/components/TheNavBar.tsx';
 
 export function Header(): ReactElement {
   return (
     <header className="flex h-16 items-center max-w-7xl px-4 sm:px-6 lg:px-8 gap-8 bg-gray-800">
-      <div className="flex gap-3 shrink-0">
+      <Link href="/" className="flex gap-3 shrink-0">
         <img
           alt="Pet Profile Generator"
           src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
           className="size-8"
         />
         <span className="text-xl text-white">PetProfiler</span>
-      </div>
+      </Link>
       <TheNavBar />
     </header>
   );
--- packages/nextjs/src/components/TheNavBar.tsx
+++ packages/nextjs/src/components/TheNavBar.tsx
@@ -1,37 +1,42 @@
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import type { ReactElement } from 'react';
 
-interface Link {
+interface NavLink {
   name: string;
   href: string;
-  current: boolean;
+}
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
 }
 
 export function TheNavBar(): ReactElement {
-  const navigation: Link[] = [
-    { name: 'Home', href: '#', current: true },
-    { name: 'Generator', href: '/generator', current: false },
+  const pathname = usePathname();
+  
+  const navigation: NavLink[] = [
+    { name: 'Home', href: '/' },
+    { name: 'Generator', href: '/generator' },
   ];
-
-  function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ');
-  }
 
   return (
     <nav className="flex justify-start gap-2">
-      {navigation.map((item: Link) => (
-        <Link
-          key={item.name}
-          href={item.href}
-          className={classNames(
-            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-            'rounded-md px-3 py-2 text-sm font-medium',
-          )}
-          aria-current={item.current ? 'page' : undefined}
-        >
-          {item.name}
-        </Link>
-      ))}
+      {navigation.map((item: NavLink) => {
+        const isCurrent = pathname === item.href;
+        return (
+          <Link
+            key={item.name}
+            href={item.href}
+            className={classNames(
+              isCurrent ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+              'rounded-md px-3 py-2 text-sm font-medium',
+            )}
+            aria-current={isCurrent ? 'page' : undefined}
+          >
+            {item.name}
+          </Link>
+        );
+      })}
     </nav>
   );
 }
